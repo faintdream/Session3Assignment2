d81b46258e2c1f1d5ff819ba4c160ebc9e3c9592
faintdream <dubey.akash@gmail.com>
2017-07-14 06:43:18 +0530

    program to demonstrate default constructor initialization

---
+++ src/example/example.java
@@ -0,0 +1,18 @@
+package example;
+class example {
+
+int x;
+
+int y;
+
+String name;
+void hello () {
+ System.out.println("Hello");
+}
+
+ public static void main(String args[]) {
+	example pnt =new example();
+	System.out.println("pnt is :" + pnt.name + " " + pnt.x + " " + pnt.y);
+ }
+}
+
