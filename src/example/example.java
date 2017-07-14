ea3e636ada3eb6f0f2794974109b3a22137bf5a0
faintdream <dubey.akash@gmail.com>
2017-07-14 07:19:16 +0530

    added comments

--- src/example/example.java
+++ src/example/example.java
@@ -1,18 +1,17 @@
 package example;
 class example {
 
-int x;
-
+int x;              //data member declaration
 int y;
-
 String name;
 void hello () {
  System.out.println("Hello");
 }
 
  public static void main(String args[]) {
-	example pnt =new example();
-	System.out.println("pnt is :" + pnt.name + " " + pnt.x + " " + pnt.y);
+	example pnt =new example();                         //data member initialization using default constructor  example() 
+                                                            // during object creation
+	System.out.println("pnt is :" + pnt.name + " " + pnt.x + " " + pnt.y); 
  }
 }
 
